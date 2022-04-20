580b61874370df183900cd1aedfcd59846da48a9
Jjiya <gpwl9610@naver.com>
2022-04-20 15:29:01 +0900

    #2.6 모듈 분리 / 랜딩 페이지 = appController  / 의존성 주입

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 /* @Module => Decorator
 .* 클래스에 함수 기능을 추가할 수 있게 해줌
 .* 클래스 위의 함수
 .*/
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {} // root Module
