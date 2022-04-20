79ba5641d8433f6f10559967d94f62ffee6f3b5f
Jjiya <gpwl9610@naver.com>
2022-04-20 09:34:13 +0900

    #1 ARCHITECTURE OF NESTJS > #1.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/* @Module => Decorator
+.* 클래스에 함수 기능을 추가할 수 있게 해줌
+.* 클래스 위의 함수
+.*/
 @Module({
   imports: [],
   controllers: [AppController],
