f45e086f98c3cec3024ee7df4f2fc018fd0fc07a
Jjiya <gpwl9610@naver.com>
2022-04-20 10:41:58 +0900

    #2.1 query(), body() 데코레이터, return json

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies') // url entry point
 export class MoviesController {
@@ -7,6 +7,14 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie with a made after: ${searchingYear}`;
+  }
+
+  // search를 위에 나두고 나니, search로 입력했을 땐 search가
+  // id를 입력했을 땐 id로 매핑됨
+  // 내 생각엔 search랑 일치하면 search타고 그 외에는 자동으로 id로 매핑되는듯
   @Get('/:id')
   getOne(@Param('id') movieId: string): string {
     // ⭐ Nest에서는 무언가 필요하면 요청해야함 ⭐
@@ -14,8 +22,8 @@ export class MoviesController {
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    return movieData;
   }
 
   @Delete('/:id')
@@ -24,7 +32,16 @@ export class MoviesController {
   }
 
   @Patch('/:id')
-  update(@Param('id') movieId: string) {
-    return `This will update a movie with the id : ${movieId}`;
+  update(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
+
+  // search가 여기 있으면 같은 Get mapping이면서 파일 상단에 있는 /:id가 실행된다.
+  // @Get('search')
+  // search() {
+  //   return `We are searching for a movie with a title`;
+  // }
 }
