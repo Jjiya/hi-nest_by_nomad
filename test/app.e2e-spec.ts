fa274d77d4bc5901af8b92157ee9982108b519e4
Jjiya <gpwl9610@naver.com>
2022-04-21 13:45:46 +0900

    #4.1 e2e 테스트 시 test application에도 동일환 환경 적용할 것!

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // beforeEach > beforeAll: 테스트 실행마다 app을 생성하는게 아닌 한 번만 생성할 수 있게끔 변경
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    // 실제 환경과 동일한 application을 만들어줄 수 있게끔 설정해야함
+    // 이것이 /movies/1로 url 파라미터를 넘겼을 때 404에러가 난 이유다.
+    // >> 자동 형변환을 해주지 않았기 떄문에 1파라미터가 string으로 함수에 전달되었고
+    // entity의 id는 number타입이기 떄문에 string으로는 조회 및 삭제가 되지 않았던 것... 두둥
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // 데코레이터 없는건 통과 못함
+        forbidNonWhitelisted: true, //whitelist에서 통과 못한 속성이 있으면 request 거부
+        transform: true, // 타입 형변환
+      }),
+    ); // dto 유효성 검사
     await app.init();
   });
 
@@ -37,7 +49,18 @@ describe('AppController (e2e)', () => {
         .expect(201);
     }); // it('(POST)')
     it('(DELETE)', () => {
-      return request(app.getHttpServer()).delete('/movies').expect(404); // id parameter 어떻게 전달하노..?
+      return request(app.getHttpServer()).delete('/movies').expect(404); // id parameter 어떻게 전달하노..? >> #4.1강의, 실제 환경과 동일한 테스트 applictaion 생성해주기.(url 파라미터 형변환)
     }); // it('(DELETE)')
+  }); // describe('/movies')
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/99').expect(404);
+    });
+    it.todo('DELETE');
+    it.todo('PATCH');
   });
 });
